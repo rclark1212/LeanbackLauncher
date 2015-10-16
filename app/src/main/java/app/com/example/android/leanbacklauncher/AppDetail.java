719b0922130f9a23ddc9cfea7de44b9e765e6577
rclark1212 <rclark1212@gmail.com>
2015-10-16 10:57:41 -0700

    hack up video sample app for tv leanback mode into a simple launcher.

---
+++ app/src/main/java/app/com/example/android/leanbacklauncher/AppDetail.java
@@ -0,0 +1,17 @@
+package app.com.example.android.leanbacklauncher;
+
+import android.content.pm.ApplicationInfo;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
+
+/**
+ * Created by rclark on 10/14/2015.
+ */
+public class AppDetail {
+    CharSequence label;
+    CharSequence name;
+    Drawable icon;
+    Resources res;
+    ApplicationInfo ai;
+    Drawable banner;
+}
